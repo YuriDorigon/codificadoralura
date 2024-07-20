c730832148bc15632b7010b83d6064b8058e113f
YuriDorigon <yuridorigon12@gmail.com>
2024-07-20 20:15:04 -0300

    up

---
+++ script/script.js
@@ -0,0 +1,6 @@
+CLIQUEI = function() {
+    var elements = document.getElementsByClassName("no-result");
+    for (var i = 0; i < elements.length; i++) {
+        elements[i].style.display = "none";
+    }
+};
