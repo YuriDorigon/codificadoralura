c0563d6f9dcecc8b57b7e31d39b8815db9c95394
YuriDorigon <yuridorigon12@gmail.com>
2024-07-20 22:01:35 -0300

    dasd

--- script/script.js
+++ script/script.js
@@ -1,6 +1,14 @@
-CLIQUEI = function() {
-    var elements = document.getElementsByClassName("no-result");
-    for (var i = 0; i < elements.length; i++) {
-        elements[i].style.display = "none";
-    }
-};
+// Função para atualizar a visibilidade dos elementos
+function enviarMensagem() {
+    document.querySelectorAll(".no_result").forEach(el => el.style.display = "none");
+    document.querySelectorAll(".result").forEach(el => el.style.display = "flex");
+
+
+    var mensagem = criptografiaDeMensagem();
+    document.getElementById("textresult").textContent = mensagem;
+}
+
+
+function criptografiaDeMensagem() {
+    return document.getElementById('text_send').value;
+}
