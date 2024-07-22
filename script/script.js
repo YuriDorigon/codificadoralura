2f71d564628eca3e92d11bcfbcf3d5f56c6f6d76
YuriDorigon <yuridorigon12@gmail.com>
2024-07-22 18:47:31 -0300

    cripter concluido

--- script/script.js
+++ script/script.js
@@ -1,36 +1,83 @@
 function criptoMensagem() {
     let mensagem = document.getElementById('text_send').value;
     let mensagemSemAcento = mensagem.normalize('NFD').replace(/[\u0300-\u036f]/g, "");
-    let criptafia  = mensagemSemAcento.replace(/e/g, "enter").replace(/i/g, "imes").replace(/a/g, "ai").replace(/o/g, "ober").replace(/u/g, "ufat");
+    let criptafia = mensagemSemAcento.replace(/e/g, "enter").replace(/i/g, "imes").replace(/a/g, "ai").replace(/o/g, "ober").replace(/u/g, "ufat");
     let resultCript = criptafia;
 
 
-    document.getElementById("result_container").innerHTML = 
-    '<textarea readonly id="result-text">' + resultCript + '</textarea>' + 
-    '<input onClick="copyMensagem()" type="button" value="copiar"></input>';
+    if (document.getElementById('text_send').value === "") {
+        const Toast = Swal.mixin({
+            toast: true,
+            position: "top-end",
+            showConfirmButton: false,
+            timer: 2000,
+            timerProgressBar: false,
+            didOpen: (toast) => {
+              toast.onmouseenter = Swal.stopTimer;
+              toast.onmouseleave = Swal.resumeTimer;
+            }
+          });
+          Toast.fire({
+            icon: "error",
+            title: "Coloque algum texto."
+          });
 
+    } else {
+        document.getElementById("result_container").innerHTML =
+            '<textarea readonly id="result-text">' + resultCript + '</textarea>' +
+            '<input onClick="copyMensagem()" type="button" value="copiar"></input>';
+    }
 }
 
-    
-function descriptMensagem(){
+function descriptMensagem() {
     let mensagem = document.getElementById('text_send').value;
-    let criptafia  = mensagem.replace(/enter/g, "e").replace(/imes/g, "i").replace(/ai/g, "a").replace(/ober/g, "o").replace(/ufat/g, "u");
+    let criptafia = mensagem.replace(/enter/g, "e").replace(/imes/g, "i").replace(/ai/g, "a").replace(/ober/g, "o").replace(/ufat/g, "u");
     let resultCript = criptafia;
 
-    document.getElementById("result_container").innerHTML = 
-    '<textarea readonly id="result-text">' + resultCript + '</textarea>' + 
-    '<input onClick="copyMensagem()" type="button" value="copiar"></input>';
+
+    if (document.getElementById('text_send').value === "") {
+        const Toast = Swal.mixin({
+            toast: true,
+            position: "top-end",
+            showConfirmButton: false,
+            timer: 2000,
+            timerProgressBar: false,
+            didOpen: (toast) => {
+              toast.onmouseenter = Swal.stopTimer;
+              toast.onmouseleave = Swal.resumeTimer;
+            }
+          });
+          Toast.fire({
+            icon: "error",
+            title: "Coloque algum texto."
+          });
+          
+    } else {
+        document.getElementById("result_container").innerHTML =
+            '<textarea readonly id="result-text">' + resultCript + '</textarea>' +
+            '<input onClick="copyMensagem()" type="button" value="copiar"></input>';
+    }
 
 }
 
 function copyMensagem() {
- var copyText = document.getElementById("result-text");
+    var copyText = document.getElementById("result-text");
+    navigator.clipboard.writeText(copyText.value);
 
- copyText.select();
+    const Toast = Swal.mixin({
+        toast: true,
+        position: "top-end",
+        showConfirmButton: false,
+        timer: 2000,
+        timerProgressBar: false,
+        didOpen: (toast) => {
+          toast.onmouseenter = Swal.stopTimer;
+          toast.onmouseleave = Swal.resumeTimer;
+        }
+      });
+      Toast.fire({
+        icon: "success",
+        title: "Texto copiado com sucesso."
+      });
 
-
- navigator.clipboard.writeText(copyText.value);
-
- // Alert the copied text
- alert("Copied the text");
-  }
+}
