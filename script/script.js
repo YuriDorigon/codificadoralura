f245d9ae0705a1b08c9851896e940ee64c3ab4e8
YuriDorigon <yuridorigon12@gmail.com>
2024-07-21 22:03:26 -0300

    adicionado as funcitions

--- script/script.js
+++ script/script.js
@@ -1,14 +1,36 @@
-// Função para atualizar a visibilidade dos elementos
-function enviarMensagem() {
-    document.querySelectorAll(".no_result").forEach(el => el.style.display = "none");
-    document.querySelectorAll(".result").forEach(el => el.style.display = "flex");
+function criptoMensagem() {
+    let mensagem = document.getElementById('text_send').value;
+    let mensagemSemAcento = mensagem.normalize('NFD').replace(/[\u0300-\u036f]/g, "");
+    let criptafia  = mensagemSemAcento.replace(/e/g, "enter").replace(/i/g, "imes").replace(/a/g, "ai").replace(/o/g, "ober").replace(/u/g, "ufat");
+    let resultCript = criptafia;
 
 
-    var mensagem = criptografiaDeMensagem();
-    document.getElementById("textresult").textContent = mensagem;
+    document.getElementById("result_container").innerHTML = 
+    '<textarea readonly id="result-text">' + resultCript + '</textarea>' + 
+    '<input onClick="copyMensagem()" type="button" value="copiar"></input>';
+
 }
 
+    
+function descriptMensagem(){
+    let mensagem = document.getElementById('text_send').value;
+    let criptafia  = mensagem.replace(/enter/g, "e").replace(/imes/g, "i").replace(/ai/g, "a").replace(/ober/g, "o").replace(/ufat/g, "u");
+    let resultCript = criptafia;
 
-function criptografiaDeMensagem() {
-    return document.getElementById('text_send').value;
+    document.getElementById("result_container").innerHTML = 
+    '<textarea readonly id="result-text">' + resultCript + '</textarea>' + 
+    '<input onClick="copyMensagem()" type="button" value="copiar"></input>';
+
 }
+
+function copyMensagem() {
+ var copyText = document.getElementById("result-text");
+
+ copyText.select();
+
+
+ navigator.clipboard.writeText(copyText.value);
+
+ // Alert the copied text
+ alert("Copied the text");
+  }
